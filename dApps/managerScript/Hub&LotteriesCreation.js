5788703f1c73055ec95c33fcae862ead1e4d90f7
Dmitry <gubanovdmit@gmail.com>
2019-12-02 15:12:40 +0300

    lottery manager replanishment

--- dApps/managerScript/Hub&LotteriesCreation.js
+++ dApps/managerScript/Hub&LotteriesCreation.js
@@ -2,13 +2,18 @@
 /// <reference path="../node_modules/@waves/js-test-env/index.d.ts" />
 const fs = require("fs");
 const lotteryInfo = []
+
+const seedWithMoney = 'scorpion early squirrel place father firm nothing warrior robot secret life blur fragile barrel task'
+const comissinForSetScript = 100000
+
+
+//create Accounts
 seedHub = wavesCrypto.randomSeed();
 addressHub = wavesCrypto.address(seedHub, 'T');
 let hub = {
     address: addressHub,
     seed :seedHub
 }
-//fs.appendFileSync("HubInfo", JSON.stringify(Hub,)+ "\n")
 
 for (i = 0; i < 12; i++) {
     seedLottery = wavesCrypto.randomSeed();
@@ -46,3 +51,16 @@ fs.writeFileSync("lotteryInfo.json", JSON.stringify(lotteryInfo,null,4))
 fs.writeFileSync("../../lottery/src/json/hub.json", JSON.stringify({address: hub.address}))
 fs.writeFileSync("../../lottery/src/json/lotteries.json", JSON.stringify(lotteryInfo.map(({address,sum}) => ({address,sum}))))
 
+const paramsForMassTransfer = {
+    transfers: [
+      {
+        amount: 100000,
+        recipient: '3P23fi1qfVw6RVDn4CH2a5nNouEtWNQ4THs',
+      },
+      {
+        amount: 200,
+        recipient: '3PPnqZznWJbPG2Z1Y35w8tZzskiq5AMfUXr',
+      },
+    ],
+
+  }
