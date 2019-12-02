45d5d6e167c205ddd9e2403512b2615919731a3d
Dmitry <gubanovdmit@gmail.com>
2019-12-02 13:53:58 +0300

    lottery script changed. fixed problem with repeated algorith

--- dApps/managerScript/Hub&LotteriesCreation.js
+++ dApps/managerScript/Hub&LotteriesCreation.js
@@ -1,14 +1,14 @@
 
 /// <reference path="../node_modules/@waves/js-test-env/index.d.ts" />
 const fs = require("fs");
-
+const file = []
 seedHub = wavesCrypto.randomSeed();
 addressHub = wavesCrypto.address(seedHub, 'T');
 let Hub = {
     address: addressHub,
     seed :seedHub
 }
-fs.appendFileSync("HubInfo", JSON.stringify(Hub)+ "\n")
+//fs.appendFileSync("HubInfo", JSON.stringify(Hub,)+ "\n")
 
 for (i = 0; i < 12; i++) {
     seedLottery = wavesCrypto.randomSeed();
@@ -18,7 +18,7 @@ for (i = 0; i < 12; i++) {
         sum : 500,
         seed :seedLottery
     }
-    fs.appendFileSync("lotteryInfo", JSON.stringify(lottery)+ "\n")
+    file.push(lottery)
 }
     
 for (i = 0; i < 6; i++) {
@@ -29,7 +29,7 @@ for (i = 0; i < 6; i++) {
             sum : 1000,
             seed :seedLottery
         }
-    fs.appendFileSync("lotteryInfo", JSON.stringify(lottery)+ "\n")
+    file.push(lottery)
 }
 for (i = 0; i < 4; i++) {
     seedLottery = wavesCrypto.randomSeed();
@@ -39,5 +39,7 @@ for (i = 0; i < 4; i++) {
         sum : 2000,
         seed :seedLottery
     }
-    fs.appendFileSync("lotteryInfo", JSON.stringify(lottery)+ "\n")
+    file.push(lottery)
   }
+  fs.writeFile("lotteryInfo", JSON.stringify(file,null,4)) 
+fs.writeFile("lotteryInfo", JSON.stringify(file,null,4))
