ab44ca6ae958c9a387c66af921ca8a57d86f6352
Alexey Nagorny <anagorny@wavesplatform.com>
2019-12-03 19:39:21 +0300

    merge texts

--- landing/js/index.js
+++ landing/js/index.js
@@ -1,6 +1,6 @@
 const langs = {
     rus: {
-        subtitleChunk1: "В течении двух месяцев разиграем",
+        subtitleChunk1: "В течении двух месяцев разыграем",
         subtitleChunk2: "70,000 WAVES",
         subtitleChunk3: "",
         fomoSubtitleChunk1: "Кто купил",
