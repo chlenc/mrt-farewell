7fb0b8f776e6828d8990a29c504c884de257cfac
Alexey Nagorny <anagorny@wavesplatform.com>
2019-12-26 07:28:52 +0300

    add exchange link

--- lottery/src/Components/Button/index.tsx
+++ lottery/src/Components/Button/index.tsx
@@ -1,4 +1,4 @@
-import { h, FunctionComponent } from 'preact';
+import { FunctionComponent, h } from 'preact';
 import styles from './styles.less';
 import cn from 'classnames';
 
@@ -11,11 +11,21 @@ export interface IButtonProps {
     disabled?: boolean
 }
 
+const ExchangeSvg = () => <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M6.99991 7.00002L13.9998 14H0L6.99991 7.00002Z" fill="white"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M7.00009 6.99998L14 0H0.00017374L7.00009 6.99998Z" fill="white"/>
+</svg>
+
+export const ExchangeLink = () => <a class={cn(styles.root, styles.link)}
+                              href="https://waves.exchange/"
+                              target="_blank">You can buy MRT here &nbsp;<ExchangeSvg/>&nbsp; Waves.Exchange</a>;
+
 const Button: FunctionComponent<IButtonProps> = ({onClick, title, style, className, disabled, action}) => {
     return <button class={cn(styles.root, className, {[styles.action]: action, [styles.disabled]: disabled})}
-                   style={style}
-                   disabled={disabled}
-                   onClick={onClick}>{title}</button>;
+                  style={style}
+                  disabled={disabled}
+                  onClick={onClick}>{title}</button>;
 };
+
 
 export default Button;
