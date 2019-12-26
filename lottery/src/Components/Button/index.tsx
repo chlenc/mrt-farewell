8971483e14b102799de40ad1075990fb28e7eade
Alexey Nagorny <anagorny@wavesplatform.com>
2019-12-26 14:39:23 +0300

    open app

--- lottery/src/Components/Button/index.tsx
+++ lottery/src/Components/Button/index.tsx
@@ -1,4 +1,4 @@
-import { h, FunctionComponent } from 'preact';
+import { FunctionComponent, h } from 'preact';
 import styles from './styles.less';
 import cn from 'classnames';
 import { logToTagManager } from '@src/utils/logToTagManager';
@@ -19,15 +19,24 @@ const Button: FunctionComponent<IButtonProps> = ({onClick, title, style, classNa
                    onClick={onClick}>{title}</button>;
 };
 
-const ExchangeSvg = () => <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+const ExchangeSvg = () => <svg width="14" height="14" viewBox="0 0 14 14" fill="none"
+                               xmlns="http://www.w3.org/2000/svg">
     <path fill-rule="evenodd" clip-rule="evenodd" d="M6.99991 7.00002L13.9998 14H0L6.99991 7.00002Z" fill="white"/>
     <path fill-rule="evenodd" clip-rule="evenodd" d="M7.00009 6.99998L14 0H0.00017374L7.00009 6.99998Z" fill="white"/>
 </svg>
 
-export const ExchangeLink = () => <a class={cn(styles.root, styles.link)}
-                                     onClick={() => logToTagManager({event: 'lotteryGoToExchange'})}
-                                     style="display: flex;align-items: center;text-decoration: none;width: 350px;justify-content: center;"
-                                     href="https://waves.exchange/"
-                                     target="_blank">You can buy MRT here &nbsp;<ExchangeSvg/>&nbsp; Waves.Exchange</a>;
+
+const exchangeClickHandler = () => {
+    logToTagManager({event: 'lotteryGoToExchange'})
+    setTimeout(function () {
+        (window as any).location = "https://waves.exchange/";
+    }, 25);
+    (window as any).location = "appname://";
+}
+
+export const ExchangeLink = () => <div class={cn(styles.root, styles.link)}
+                                       onClick={exchangeClickHandler}
+                                       style="display: flex;align-items: center;text-decoration: none;width: 350px;justify-content: center;">
+    You can buy MRT here &nbsp;<ExchangeSvg/>&nbsp; Waves.Exchange</div>;
 
 export default Button;
